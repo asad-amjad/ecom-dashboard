f95fac45e6efe014dbe5ed4fe60c68a030add6d2
asad-amjad <asad.amjad.pk@gmail.com>
2022-07-28 23:31:08 +0500

    signUp basic

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,7 +1,55 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div><h4>Register </h4></div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submit = () => {
+    console.log(name, email, password);
+  };
+
+  return (
+    <div className="register">
+      <h4>Register </h4>
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <input
+        className="inputBox"
+        type="email"
+        value={email}
+        placeholder="Email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        className="inputBox"
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button
+        className="saveButton"
+        type="button"
+        onClick={() => {
+          submit();
+        }}
+      >
+        Sign up
+      </button>
+    </div>
+  );
 };
 
 export default SignUp;
