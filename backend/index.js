104d9b81e2a5e3090228eb8e93ccc21e78c22b43
asad-amjad <asad.amjad.pk@gmail.com>
2022-07-31 12:33:25 +0500

    login API

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,23 @@ app.use(cors());
 app.post("/register", async (req, res) => {
   const user = new User(req.body);
   const result = await user.save();
+  result = result.toObject();
+  delete result.password;
   res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+  console.log();
+  let user = await User.findOne(req.body).select("-password");
+  if (req.body.email && req.body.password) {
+    if (user) {
+      res.send(user);
+    } else {
+      res.send("User Not found");
+    }
+  } else {
+    res.send("Need both fields to login");
+  }
 });
 
 app.listen(5000);
