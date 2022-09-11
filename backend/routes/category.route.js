747aa2a540529be67396a0c75b9801eb17e1792a
asad-amjad <asad.amjad.pk@gmail.com>
2022-09-11 04:23:35 -0700

    api's auth

--- backend/routes/category.route.js
+++ backend/routes/category.route.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/category.controller");
+const middlewares = require("../middlewares/authJWT");
 
-router.get("/", controller.all);
-router.post("/add", controller.add);
-router.get("/:id", controller.details);
-router.delete("/:id/delete", controller.delete);
-router.put("/:id/update", controller.update);
+router.get("/", [middlewares.authenticateToken], controller.all);
+router.post("/add", [middlewares.authenticateToken], controller.add);
+router.get("/:id", [middlewares.authenticateToken], controller.details);
+router.delete(
+  "/:id/delete",
+  [middlewares.authenticateToken],
+  controller.delete
+);
+router.put("/:id/update", [middlewares.authenticateToken], controller.update);
 
 module.exports = router;
